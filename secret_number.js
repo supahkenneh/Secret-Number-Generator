0dd55f525d4480b8a723186200282e71b91bebd0
Kenny Chung <kcmac@Kennys-MacBook-Pro.local>
2018-07-02 18:29:32 -1000

    solution

--- secret_number.js
+++ secret_number.js
@@ -1,4 +1,11 @@
 'use strict';
 module.exports = function() {
-  
+
+var secretNumber = Math.floor(Math.random()*1000001);
+
+function randomNumber(){
+    return secretNumber;
+}
+
+return randomNumber;
 };
